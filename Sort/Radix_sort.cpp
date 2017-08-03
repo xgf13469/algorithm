303d3cfef5621218077caaa3e8cad4ae62a5c3bc
xugf <xgf13469@github.com>
2017-08-03 11:26:09 +0800

    基数排序 LSD 和 MSDD

--- Sort/Radix_sort.cpp
+++ Sort/Radix_sort.cpp
@@ -23,25 +23,54 @@ int GetNumInPos(int num, int pos)
 }
 
 #define MAXPOS 10    //最高位的位数
+
+//LSD:从个位开始到最高位数
 void Radix_sort(vector<int> &A)
 {
 	int len = A.size();
-	vector<queue<int>> radixArray(10);  //分为0~9的序列空间，用队列保存每个桶分配的数据
+	vector<vector<int>> radixArray(10);  //分为0~9的序列空间
 
 	for (int pos = 1; pos <= MAXPOS; pos++)    //从个位开始到最高位数
 	{
 		for (int i = 0; i < len; i++)    //分配过程
 		{
 			int num = GetNumInPos(A[i], pos);
-			radixArray[num].push(A[i]);
+			radixArray[num].push_back(A[i]);
 		}
 
 		for (int i = 0, j = 0; i < 10; i++)    //收集
 		{
 			while (!radixArray[i].empty())
 			{
+				A[j++] = radixArray[i].front();		//取首部数据依次插入原数组
+				radixArray[i].erase(radixArray[i].begin());		//移除首部元素
+			}
+		}
+	}
+}
+
+//MSD:从最高位开始到个位
+void RadixSort(vector<int> &A, int d)
+{
+	int len = A.size();
+	vector<vector<int>> radixArray(10);  //分为0~9的序列空间，用队列保存每个桶分配的数据
+
+	//位数大于0，且数组长度大于1
+	if (d >= 1 && len > 1)
+	{
+		for (int i = 0; i < len; i++)    //分配过程
+		{
+			int num = GetNumInPos(A[i], d);
+			radixArray[num].push_back(A[i]);
+		}
+		cout << endl;
+		for (int i = 0, j = 0; i < 10; i++)    //收集
+		{
+			RadixSort(radixArray[i], d-1);		//递归，对子桶从次高位开始分配
+			while (!radixArray[i].empty())
+			{
 				A[j++] = radixArray[i].front();		//取队列首部数据依次插入原数组
-				radixArray[i].pop();
+				radixArray[i].erase(radixArray[i].begin());
 			}
 		}
 	}
@@ -55,8 +84,13 @@ int main()
 	Radix_sort(a);
 	
 	for (auto it : a)
-		cout << it << "  ";
+		cout << "LSD :" << it << "  ";
 
+	Radix_sort(a, 2);
+	
+	for (auto it : a)
+		cout << "MSD :" << it << "  ";
+	
 	system("pause");
 	return 0;
 }
