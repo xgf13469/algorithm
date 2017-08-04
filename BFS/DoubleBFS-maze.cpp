f32b830bfdac53fea5808106609690255406c242
xugf <xgf13469@github.com>
2017-08-04 15:15:06 +0800

    广度优先-迷宫

--- BFS/DoubleBFS-maze.cpp
+++ BFS/DoubleBFS-maze.cpp
@@ -4,124 +4,164 @@
 
 using namespace std;
 
-/*
-迷宫程序
-仅仅输出达到迷宫出口的最短路径长度，不输出最短的路径！！！！
-*/
-
 struct Node
 {
 	int x;
 	int y;
-	Node(int _x, int _y) :x(_x), y(_y){}
+	int visitType;
+	Node *pre;
+	Node(int _x, int _y) :x(_x), y(_y),visitType(0), pre(NULL){}
 };
+
 class Maze
 {
 public:
-	Maze(vector<vector<int>> _maze) :maze(_maze),visit(vector<vector<int>>(_maze.size(),vector<int>(_maze.size(),0))), found(false),step(0){}
-	int GetPath(Node &start, Node &finish);
+	Maze(vector<vector<int>> _maze) :maze(_maze){}
+	vector<Node*> GetPath(Node &start, Node &finish);
 private:
 	vector<vector<int>> maze;		//地图
-	vector<vector<int>> visit;
-	bool found;		//找到路径标识
-	int step;		//步数
-	queue<Node> frontQueue;		//前向访问队列
-	queue<Node> backQueue;		//反向访问队列
-	
-	//双向扩展
-	void search(queue<Node> &);
-	//节点可行性
-	bool isValid(const Node &);
-	//获取指定节点周围的可行节点
-	vector<Node> GetAround(const Node &);
+
+	vector<Node*> visited;			//保存被访问过的节点
+	queue<Node*> frontQueue;		//前向访问队列
+	queue<Node*> backQueue;		//反向访问队列
+	vector<Node*> path;			//到达终点的路径
+
+	void search(queue<Node*> &);		//搜索队列中节点的相邻节点
+	bool isValid(const Node *);			//节点是否有效：是否在地图中；是否有障碍
+	vector<Node*> GetAround(const Node *);			//获取指定节点周围的可行节点
+	Node *isVisited(Node *);			//判断节点是否被访问过：是返回该节点，否返回NULL
 };
 
-int Maze::GetPath(Node &start, Node &finish)
+vector<Node*> Maze::GetPath(Node &start, Node &finish)
 {
-	visit[start.x][start.y] = 1;
-	visit[finish.x][finish.y] = 2;
-	frontQueue.push(start);
-	backQueue.push(finish);
+	start.visitType = 1;
+	finish.visitType = 2;
 
-	while (!frontQueue.empty() || !backQueue.empty())
+	frontQueue.push(&start);
+	backQueue.push(&finish);
+
+	visited.push_back(&start);
+	visited.push_back(&finish);
+
+	while (!frontQueue.empty() && !backQueue.empty())
 	{
-		if (!frontQueue.empty() && (frontQueue.size() < backQueue.size()))
-			search(frontQueue);		//扩展前向搜索队列
+		if (frontQueue.size() < backQueue.size())
+			search(frontQueue);		//前向搜索
 		else
-			search(backQueue);		//扩展后向搜索队列
+			search(backQueue);		//后向搜索
 
-		if (found)
-			return step + 1;
+		if (!path.empty())
+			break;
 	}
-	return -1;
+	return path;
 }
 
-void Maze::search(queue<Node> &que)
+void Maze::search(queue<Node*> &que)
 {
 	int n = que.size();		//遍历这个层次的所有节点
 	for (int i = 0; i < n; i++)
 	{
-		Node cur = que.front();		//从队列中弹出当前搜索的点
+		Node *cur = que.front();		//从队列中弹出当前搜索的点
 		que.pop();
-		vector<Node> around = GetAround(cur);		//搜索节点周围可行的节点
+
+		vector<Node*> around = GetAround(cur);		//搜索节点周围可行的节点
 
 		for (auto &node : around)
 		{
-			if (visit[node.x][node.y] == visit[cur.x][cur.y])
+			if (node->visitType==cur->visitType)
 				continue;
-			else if (visit[node.x][node.y] == 0)
+			else if (node->visitType==0)
 			{
-				visit[node.x][node.y] = visit[cur.x][cur.y];
+				node->visitType=cur->visitType;
+				node->pre = cur;
 				que.push(node);
+				visited.push_back(node);
 			}
+
+			//另一个方向访问过的节点与当前节点相邻
 			else
 			{
-				found = true;
+				//cur 和 node 为两个方向的相邻节点
+				if (cur->visitType == 2)
+					swap(cur, node);
+
+				//保存路径
+				while (cur)
+				{
+					path.insert(path.begin(), cur);
+					cur = cur->pre;
+				}
+				while (node)
+				{
+					path.push_back(node);
+					node = node->pre;
+				}
 				return;
 			}
 		}
 	}
-	step++;		//每遍历一个层次，步数加1
 }
-bool Maze::isValid(const Node &node)
+bool Maze::isValid(const Node *node)
 {
-	if (node.x<0 || node.x > maze.size() - 1 || node.y<0 || node.y > maze.size() - 1
-		|| maze[node.x][node.y])
+	if (node->x<0 || node->x > maze.size() - 1 || node->y<0 || node->y > maze[0].size() - 1
+		|| maze[node->x][node->y])
 		return false;
 	return true;
 }
-vector<Node> Maze::GetAround(const Node &node)
+vector<Node*> Maze::GetAround(const Node *node)
 {
 	int d[4][2] = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };	//上下左右的点
 
-	vector<Node> around;
+	vector<Node*> around;
 	for (int i = 0; i < 4; i++)
 	{
-		int newx = node.x + d[i][0];
-		int newy = node.y + d[i][1];
-		if (isValid(Node(newx, newy)))
-			around.push_back(Node(node.x + d[i][0], node.y + d[i][1]));
+		Node *temp = new Node(node->x + d[i][0], node->y + d[i][1]);
+		if (isValid(temp))
+		{
+			auto node = isVisited(temp);
+			if (node)
+				around.push_back(node);
+			else
+				around.push_back(temp);
+		}
 	}
-		
 	return around;
+}
+Node *Maze::isVisited(Node *node)
+{
+	for (auto &it : visited)
+	{
+		if (node->x == it->x && node->y == it->y)
+			return it;
+	}
+	return NULL;
 }
 
 int main()
 {
 	vector<vector<int>> maze = {
-		{ 0, 1, 0, 0, 0 },
-		{ 0, 1, 0, 1, 0 },
-		{ 0, 0, 0, 0, 0 },
-		{ 0, 1, 1, 1, 0 },
-		{ 0, 0, 0, 1, 0 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1 },
+		{ 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0 },
+		{ 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+		{ 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0 },
+		{ 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 }
 	};
-
-	Node start(0, 0);
-	Node finish(4, 4);
-	Maze m(maze);
 	
-	cout << m.GetPath(start, finish);
+	Node start(0, 0);
+	Node finish(7, 11);
+	Maze m(maze);
 
+	auto path = m.GetPath(start, finish);
+	if (path.empty())
+		cout << "no path!" << endl;
+	for (auto node : path)
+	{
+		cout << "(" << node->x << "," << node->y << ")" << endl;
+		node = node->pre;
+	}
 	system("pause");
 	return 0;
 }
